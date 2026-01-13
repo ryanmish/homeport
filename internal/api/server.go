6971b9a6744ca0d8ba50c87c7fe247192432f0f4
ryanmish <ryan@pinpointinteractive.com>
2026-01-13 16:25:49 -0500

    Remove redundant home button from VS Code header

--- internal/api/server.go
+++ internal/api/server.go
@@ -875,11 +875,6 @@ func (s *Server) serveCodeServerWrapper(w http.ResponseWriter, r *http.Request) 
                     </button>
                 </div>
             </div>
-            <a href="/" class="header-btn">
-                <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
-                    <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"/>
-                </svg>
-            </a>
         </div>
     </header>
 
